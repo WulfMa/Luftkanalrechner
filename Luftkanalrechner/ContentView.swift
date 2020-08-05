80cc72e746f99ec0c671f5f5aa3809c98acf063d
WulfMa <martinwulf@me.com>
2020-08-05 22:53:57 +0200

    added pictures, launchscreen not working. added calculation for Geschwindigkeit on base of Kanal and Volumenstrom. Removed Button Berechnen and result text fields

--- Luftkanalrechner/ContentView.swift
+++ Luftkanalrechner/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
     @State private var VolumenstromErgebnis: String = ""
     @State private var GeschwindigkeitErgebnis: String = ""
     @State private var KanalAErgebnis : String = ""
-    @State private var KanalBErgebnis : String = ""
+    @State private var KanalBEingabe : String = ""
     @State private var KanalDErgebnis : String = ""
     
     
@@ -74,23 +74,26 @@ struct ContentView: View {
                               onCommit : { self.BerechungVundG()} )
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 100, height: 50)
-                        .keyboardType(.numberPad)
-                    TextField("", text: $GeschwindigkeitEingabe)
+                    //.keyboardType(.numberPad) currently not working
+                    TextField("", text: $GeschwindigkeitEingabe,
+                              onCommit: {self.BerechungVundG()})
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 100, height: 50)
-                        .keyboardType(.numberPad)
-                    TextField("", text: $KanalAEingabe)
+                    //.keyboardType(.numberPad) currently not working
+                    TextField("", text: $KanalAEingabe,
+                              onCommit: { self.BerechungVundA()})
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 100, height: 50)
-                        .keyboardType(.numberPad)
-                    TextField("1000", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    //.keyboardType(.numberPad) currently not working
+                    TextField("1000", text: $KanalBEingabe,
+                    onCommit: { self.BerechungVundA()})
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 100, height: 50)
-                        .keyboardType(.numberPad)
+                    //.keyboardType(.numberPad) currently not working
                     TextField("1000", text: $KanalDErgebnis)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 100, height: 50)
-                        .keyboardType(.numberPad)
+                    //.keyboardType(.numberPad) currently not working
                     
                 }
                 
@@ -120,60 +123,48 @@ struct ContentView: View {
                 }
                 
             }
-            Button(action: {self.BerechungVundG()}) {
-                Text("Berechnen")
-            }
-            Spacer()
-                .frame(height: 50)
+
+      Spacer()
+        .frame(height: 100
+            )
             
-            
-            
-            HStack{
-                
-                
-                VStack(alignment: .leading, spacing: 5){
-                    
-                    Text("Volumenstrom =  \(VolumenstromErgebnis) m³/h")
-                    Text("Geschwindigkeit =  \(GeschwindigkeitErgebnis) m/s")
-                    Text("Kanal A =  \(KanalAErgebnis) mm")
-                    Text("Kanal B =  \(KanalBErgebnis) mm")
-                    Text("Rochdurchmesser D =  \(KanalDErgebnis) mm")
-                    
-                    
-                }
-                
-            }
-            
-            
-        }
-        
-        
-        
-        
+            Image("KanalWhite")
+            .resizable()
+                .frame(width: 150, height: 150)
+            Text("Luftkanarechner v0.2")
     }
-    func BerechungVundG(){
+
+    
+}
+func BerechungVundG(){
+    self.hideKeyboard()
+    var calculation : Double {
+        guard let V = Double(VolumenstromEingabe), let g = Double(GeschwindigkeitEingabe), let A = Double(KanalAEingabe) else { return 0 }
+        let product = V / g / A * 10000 / 36
+        return product
+    }
+    let formatted = String(format: "%.2f", calculation)
+    KanalBEingabe = String(formatted)
+   
+    var calculation2 : Double {
+        guard let V = Double(VolumenstromEingabe), let g = Double(GeschwindigkeitEingabe) else { return 0 }
+        let product2 = (V / g / 3600) / 3.14159265
+        let product3 = sqrt(product2) * 2000
+        return product3
+    }
+    let formatted2 = String(format: "%.2f", calculation2)
+    KanalDErgebnis = String(formatted2)
+    
+}
+    func BerechungVundA(){
         self.hideKeyboard()
         var calculation : Double {
-            guard let V = Double(VolumenstromEingabe), let g = Double(GeschwindigkeitEingabe), let A = Double(KanalAEingabe) else { return 0 }
-            let product = V / g / A * 10000 / 36
+            guard let V = Double(VolumenstromEingabe), let B = Double(KanalBEingabe), let A = Double(KanalAEingabe) else { return 0 }
+            let product = V / B / A * 10000 / 36
             return product
         }
         let formatted = String(format: "%.2f", calculation)
-        KanalBErgebnis = String(formatted)
-        var calculation2 : Double {
-            guard let V = Double(VolumenstromEingabe), let g = Double(GeschwindigkeitEingabe) else { return 0 }
-            let product2 = (V / g / 3600) / 3.14159265
-            let product3 = sqrt(product2) * 2000
-            return product3
-        }
-        let formatted2 = String(format: "%.2f", calculation2)
-        KanalDErgebnis = String(formatted2)
-        
-        
-        KanalAErgebnis = KanalAEingabe
-        GeschwindigkeitErgebnis = GeschwindigkeitEingabe
-        VolumenstromErgebnis = VolumenstromEingabe
-        
+        GeschwindigkeitEingabe = String(formatted)
     }
 }
 
