47f77037b5ce2f79c45302afcc2d5b00ef2bbba0
WulfMa <martinwulf@me.com>
2020-08-18 21:57:21 +0200

    close to final

--- Luftkanalrechner/Views/Infoscreen.swift
+++ Luftkanalrechner/Views/Infoscreen.swift
@@ -8,48 +8,124 @@
 
 import SwiftUI
 
-
-
-
 struct InfoScreen: View {
+    @State private var showingInfoSheet2 = false
     
     var body: some View {
         
-        ZStack{
+        VStack{
             
-            VStack (alignment: .center){
+            HStack (alignment: .center){
                 
-                HStack {
+                VStack (alignment: .leading){
                     
-                    VStack{
-                        Spacer()
-                            .frame(height: 20)
-                        Text("Wilkommen")
-                            .font(.title)
-                        Text("beim Kanalrechner")
-                            .font(.subheadline)
-                        Spacer()
-                            .frame(height: 20)
-                        Image("Blank")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                  
-                    }
-
+                    Text("Wilkommen")
+                        .font(.title)
+                    Text("beim Kanalrechner")
+                        .font(.subheadline)
                 }
                 
-               
+                
             }
-            
-            
+            VStack{
+                
+                Image("Blank")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Button(action: {self.showingInfoSheet2 = true}) {
+                Text("Weiter")
+                Image(systemName: "wind")
+            }
+            .sheet(isPresented: $showingInfoSheet2, content: {
+                InfoScreen2()
+            })
         }
+    }
+}
+struct InfoScreen2: View {
+    @State private var showingInfoSheet3 = false
+    
+    var body: some View {
         
-        
+        VStack{
+            
+            HStack (alignment: .center){
+                
+                VStack (alignment: .leading, spacing: 5){
+                    
+                    
+                    Text("Einführung")
+                        .font(.title)
+                        .padding(.top)
+                        .padding(.leading)
+                    Text("Zur Berechung eines Kanals Volumenstrom und Geschwindigkeit eingeben")
+                        .font(.subheadline)
+                        .padding(.bottom)
+                        .padding(.leading)
+                }
+                
+            }
+            VStack{
+                
+                Image("VundG")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Button(action: {self.showingInfoSheet3 = true}) {
+                Text("Weiter")
+                Image(systemName: "wind")
+            }
+            .sheet(isPresented: $showingInfoSheet3, content: {
+                InfoScreen3()
+            })
+                .padding()
+        }
     }
 }
 
+struct InfoScreen3: View {
+    @State private var showingInfoSheet3 = false
+    
+    var body: some View {
+        
+        VStack{
+            
+            HStack (alignment: .center){
+                
+                VStack (alignment: .leading, spacing: 5){
+                    
+                    Text("Einführung")
+                        .font(.title)
+                        .padding(.top)
+                        .padding(.leading)
+                    Text("Eine Eingabe im Feld Kanal A verändert Kanal B bei gleichbleibendem Volumenstrom und Geschwindigkeit")
+                        .font(.subheadline)
+                        .padding(.bottom)
+                        .padding(.leading)
+                }
+                
+                
+            }
+            VStack{
+                
+                Image("Blank")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Button(action: {self.showingInfoSheet3 = true}) {
+                Text("Weiter")
+                Image(systemName: "wind")
+            }
+            .sheet(isPresented: $showingInfoSheet3, content: {
+                InfoScreen3()
+            })
+                .padding()
+        }
+    }
+}
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        InfoScreen()
+        InfoScreen3()
     }
 }
